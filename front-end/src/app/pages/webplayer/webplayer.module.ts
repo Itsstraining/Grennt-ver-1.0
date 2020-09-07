afbfe9fd6c4d9b3bbd65aff4f93464709ea1b886
Huy <phamlehuy116@gmail.com>
2020-09-07 16:43:06 +0700

    add track

--- front-end/src/app/pages/webplayer/webplayer.module.ts
+++ front-end/src/app/pages/webplayer/webplayer.module.ts
@@ -3,13 +3,28 @@ import { CommonModule } from '@angular/common';
 
 import { WebplayerRoutingModule } from './webplayer-routing.module';
 import { WebplayerComponent } from './webplayer.component';
+import { ComponentsModule } from 'src/app/components/components.module';
+import { NbUserModule, NbCardModule, NbListModule, NbIconModule, NbTooltipModule, NbContextMenuModule, NbLayoutModule, NbSidebarModule } from '@nebular/theme';
+import { TrackComponent } from './track/track.component';
 
 
 @NgModule({
-  declarations: [WebplayerComponent],
+  declarations: [WebplayerComponent,TrackComponent],
   imports: [
     CommonModule,
-    WebplayerRoutingModule
+    WebplayerRoutingModule,
+    ComponentsModule,
+    NbCardModule,
+    NbUserModule,
+    NbListModule,
+    NbIconModule,
+    NbTooltipModule,
+    NbContextMenuModule,
+    NbLayoutModule,
+    NbSidebarModule,
+  ],
+  exports: [
+    TrackComponent
   ]
 })
 export class WebplayerModule { }
