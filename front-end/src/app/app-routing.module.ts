7a537b12f18aceb57f8f83df613e2c6f47fa9a26
duc <hohuynhduca7bh123@gmail.com>
2020-09-08 14:15:47 +0700

    delete some module

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from './guards/auth.guard';
-import { UploadComponent } from './pages/upload/upload.component';
 
 
 const routes: Routes = [
@@ -10,9 +9,9 @@ const routes: Routes = [
   // { path: 'library', loadChildren: () => import('./pages/library/library.module').then(m => m.LibraryModule), canActivate: [AuthGuard] },
   // { path: 'search', loadChildren: () => import('./pages/search/search.module').then(m => m.SearchModule) },
   // { path: 'playlist/:id', loadChildren: () => import('./pages/playlist/playlist.module').then(m => m.PlaylistModule), canActivate: [AuthGuard] },
-  { path: 'homepage', loadChildren: () => import('./pages/homepage/homepage.module').then(m => m.HomepageModule) },
-  { path: 'webplayer', loadChildren: () => import('./pages/webplayer/webplayer.module').then(m => m.WebplayerModule), canActivate: [AuthGuard], },
   // { path: 'webPlayer:id', loadChildren: () => import('./pages/web-player/web-player.module').then(m => m.WebPlayerModule) },
+  { path: 'homepage', loadChildren: () => import('./pages/homepage/homepage.module').then(m => m.HomepageModule) },
+  { path: 'webplayer', loadChildren: () => import('./pages/webplayer/webplayer.module').then(m => m.WebplayerModule)},
 ];
 
 @NgModule({
