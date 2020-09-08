d714346c0960f49f602396821ec9220de27afe91
duc <hohuynhduca7bh123@gmail.com>
2020-09-08 11:25:48 +0700

    toang 3

--- front-end/src/app/pages/webplayer/webplayer.module.ts
+++ front-end/src/app/pages/webplayer/webplayer.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { WebplayerRoutingModule } from './webplayer-routing.module';
 import { WebplayerComponent } from './webplayer.component';
 import { ComponentsModule } from 'src/app/components/components.module';
-import { NbUserModule, NbCardModule, NbListModule, NbIconModule, NbTooltipModule, NbContextMenuModule, NbLayoutModule, NbSidebarModule } from '@nebular/theme';
+import { NbUserModule, NbCardModule, NbListModule, NbIconModule, NbTooltipModule, NbContextMenuModule, NbLayoutModule, NbSidebarModule, NbProgressBarModule, NbButtonModule } from '@nebular/theme';
 import { TrackComponent } from './track/track.component';
+import { PlaylistComponent } from './playlist/playlist.component';
+import { UploadTaskComponent } from './upload-task/upload-task.component';
 
 
 @NgModule({
-  declarations: [WebplayerComponent,TrackComponent],
+  declarations: [WebplayerComponent,TrackComponent, PlaylistComponent, UploadTaskComponent],
   imports: [
     CommonModule,
     WebplayerRoutingModule,
@@ -22,6 +24,15 @@ import { TrackComponent } from './track/track.component';
     NbContextMenuModule,
     NbLayoutModule,
     NbSidebarModule,
+    NbProgressBarModule,
+    NbButtonModule,
+    NbCardModule,
+    NbListModule,
+    NbListModule,
+    WebplayerModule,
+    NbLayoutModule,
+    ComponentsModule,
+    NbSidebarModule
   ],
   exports: [
     TrackComponent
