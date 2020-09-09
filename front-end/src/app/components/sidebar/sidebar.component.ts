444f738d478e1111958f6506cd3fdfe5dee8ff09
Xenon <proday10x@gmail.com>
2020-09-09 15:54:23 +0700

    abc

--- front-end/src/app/components/sidebar/sidebar.component.ts
+++ front-end/src/app/components/sidebar/sidebar.component.ts
@@ -22,7 +22,7 @@ export class SidebarComponent implements OnInit, OnDestroy {
     {
       title: "Home",
       icon: "home-outline",
-      link: ['/webplayer'],
+      link: '/',
       skipLocationChange: true
     },
     {
