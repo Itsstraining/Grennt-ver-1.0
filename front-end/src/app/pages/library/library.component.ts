917041dd55fc519e67843fb501fa85e00fd77f92
Huy <phamlehuy116@gmail.com>
2020-09-09 14:10:11 +0700

    HuyFix2

--- front-end/src/app/pages/library/library.component.ts
+++ front-end/src/app/pages/library/library.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { CloudService } from '../../services/cloud.service'
 @Component({
   selector: 'app-library',
   templateUrl: './library.component.html',
@@ -7,9 +7,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LibraryComponent implements OnInit {
 
-  constructor() { }
+  @Input() users;
+  @Input()
+  playlistName;
+  folderUsers : Array<any> = [];
+
+  constructor(public cloudService: CloudService) { }
 
   ngOnInit() {
   }
-
+  isInFolderUser(users) {
+    for (const folder of this.folderUsers) {
+      if (users.id === folder.id) {
+        return true;
+      }
+    }
+    return false;
+  }
+  removeFromFolder(){
+    let arr = this.playlistName.Song.map(x => x);
+    arr.length = 0;
+    for (const Song of this.playlistName.Song) {
+      if (this.users.name !== Song.name) {
+        arr.push(Song);
+      }
+    }
+    this.playlistName.Song = arr;
+    if (this.users != null) {
+      this.cloudService
+        .addSongToPlaylist(
+          this.users,
+          this.playlistName,
+          this.playlistName.title
+        )
+        .then(result => {
+          console.log("DONE");
+        })
+        .catch(err => {
+          console.log("ERROR");
+        });
+    }
+  }
 }
